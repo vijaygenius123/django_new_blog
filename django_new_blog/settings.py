10403fd9f72e39db20d1293f1421dcb6909818f8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-01-09 21:27:18 +0530

    Added Post Model In admin.py

--- django_new_blog/settings.py
+++ django_new_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog.apps.BlogConfig'
 ]
 
 ROOT_URLCONF = 'django_new_blog.urls'
